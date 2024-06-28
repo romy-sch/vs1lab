3b25a24186a21189b11749a9d0aab31d13d10d2b
Romy <romy.schuebel@gmail.com>
2024-06-28 20:50:59 +0200

    done

--- Aufgabe3/gta_v3/models/geotag-store.js
+++ Aufgabe3/gta_v3/models/geotag-store.js
@@ -23,10 +23,89 @@
  * - The proximity constrained is the same as for 'getNearbyGeoTags'.
  * - Keyword matching should include partial matches from name or hashtag fields. 
  */
+const GeoTag = require('./geotag'); 
+const GeoTagExamples = require('./geotag-examples');
+
 class InMemoryGeoTagStore{
 
     // TODO: ... your code here ...
+    
+    #geotagList=[];
+   
+    addGeoTag(name, latitude, longitude, hashtag) {
+        const newGeoTag = new GeoTag(name, latitude, longitude, hashtag);
+        this.#geotagList.push(newGeoTag);
+    }
 
+   removeGeoTag(name){
+        for(let i=0;i<geotagList.length;i++){
+            if(name===geotagList[i].name) {
+                geotagList.splice(i,1);
+                break;
+            }
+        };
+    }
+
+    getNearbyGeoTags(latitude,longitude){
+        const radius=5;
+
+        function haversineDistanceFormula(lat1, lon1, lat2, lon2){
+            
+            // distance between latitudes and longitudes
+            let distanceLat = (lat2 - lat1) * Math.PI / 180.0;
+            let distanceLon = (lon2 - lon1) * Math.PI / 180.0;
+           
+            // convert to radians
+            lat1 = (lat1) * Math.PI / 180.0;
+            lat2 = (lat2) * Math.PI / 180.0;
+         
+            //formula
+            let a = Math.pow(Math.sin(distanceLat / 2), 2) + 
+                   Math.pow(Math.sin( distanceLon / 2), 2) * 
+                   Math.cos(lat1) * 
+                   Math.cos(lat2);
+
+            let earthRad = 6371; //earth radius in km
+            let c = 2 * Math.asin(Math.sqrt(a));
+            return earthRad * c;
+        }
+
+        
+         return this.#geotagList.filter(tag=>//returns filtered array for set radius
+            haversineDistanceFormula(latitude,longitude,tag.latitude,tag.longitude)<=radius
+            );
+        
+    }
+    
+    searchNearbyGeoTags(latitude,longitude,keyword){
+        
+        const nearbyTags = this.getNearbyGeoTags(latitude,longitude);
+        
+        const lowerKeyword = keyword.toLowerCase();
+        
+        const matching= nearbyTags.filter(tag =>
+            tag.name.toLowerCase().includes(lowerKeyword) || tag.hashtag.toLowerCase().includes(lowerKeyword)
+            );
+        
+            return matching;
+    }
+    
+    getStore(){
+        return this.#geotagList;
+    }
+
+    populate(){
+        const exampleList = GeoTagExamples.tagList;
+
+        
+        exampleList.forEach(tag => {
+            const [name, latitude, longitude, hashtag] = tag;
+            
+            this.addGeoTag(name, latitude, longitude, hashtag);
+        });
+    console.log('GeoTags populated:', this.#geotagList); 
+
+    }
 }
 
 module.exports = InMemoryGeoTagStore
